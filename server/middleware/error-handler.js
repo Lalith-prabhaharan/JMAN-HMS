a303330aa92e98c4492f40a4424fbafc73701427
VishalRaja1207 <vishalthorali@gmail.com>
2024-02-01 13:06:40 +0530

    login serverside completed

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,10 +1,9 @@
 const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({ msg: err.message })
+    }
+    return res.status(500).json({ err: 'Internal server error' })
 }
 
 module.exports = errorHandlerMiddleware
