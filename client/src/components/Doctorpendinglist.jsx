13ffe41394409ac755b4dc78b3513b0faecdb7e8
lalith kishore <lalitkishorep.20cse@kongu.edu>
2024-02-20 21:58:06 +0530

    Changed table in pending page

--- client/src/components/Doctorpendinglist.jsx
+++ client/src/components/Doctorpendinglist.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../style/Doctorpendinglist.css';
 import { DoctorNav } from './DoctorNav';
 import { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import axiosInstance from '../interceptor/axios-config';
 import { useState } from 'react';
 import { doctorpending } from '../services/services';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 export default function Doctorpendinglist() {
-    const [pendingList, setPendingList] = useState([])
+    const [pendingList, setPendingList] = useState([""])
+    const navigate=useNavigate();
     useEffect(() => {
         doctorpending()
         .then(response=>{
@@ -19,13 +22,26 @@ export default function Doctorpendinglist() {
         })
     })
 
+    const viewButton=(pending)=>{
+        console.log(pending.applicaton_id)
+        return <Link className="status1" to={`/pending/${pending.application_id}`} state={{data:pending}}>View</Link>
+    }
+
+
+    const approveButton=()=>{
+        return <button id='btn1doc' >Approve</button>
+    }
+    const rejectButton=()=>{
+       return <button id='btn2doc'>Reject</button>
+    }
+
     const id=useParams();
 
     return (
         <DoctorNav>
             <div className='status'>
             <h2>Pending Patients List</h2>
-                    <table>
+                    {/* <table>
                         <thead>
                             <tr>
                                 <th>S. No.</th>
@@ -49,7 +65,15 @@ export default function Doctorpendinglist() {
                                 
                             }
                         </tbody>
-                    </table>
+                    </table> */}
+                    <DataTable removableSort paginator rows={10} value={pendingList}>
+                        <Column field="application_id" alignHeader={'center'} sortable header="Application ID"></Column>
+                        <Column field="name" alignHeader={'center'} sortable header="Name"></Column>
+                        <Column  alignHeader={'center'}  body={viewButton}  header="Status"></Column>
+                        <Column  alignHeader={'center'}  body={approveButton}  header="Status"></Column>
+                        <Column  alignHeader={'center'}  body={rejectButton}  header="Status"></Column>
+                    </DataTable>
+                    
             </div>
         </DoctorNav>
     )
