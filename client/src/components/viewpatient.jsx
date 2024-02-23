d4a56b456c015172263b99e1710732af3be0963f
VishalRaja1207 <vishalthorali@gmail.com>
2024-02-23 17:00:32 +0530

    Approve reject added

--- client/src/components/viewpatient.jsx
+++ client/src/components/viewpatient.jsx
@@ -2,8 +2,6 @@ import "../style/viewpatient.css"
 import { DoctorNav } from '../components/DoctorNav'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
 import React, { useEffect, useState } from 'react'
 import { doctorhandling} from "../services/services";
 export const Viewpatient = () => {
@@ -20,12 +18,14 @@ export const Viewpatient = () => {
     <DoctorNav>
       <div className="status">
       <h1 className='heading'>Handling Patients</h1>
+          {patientList.length >= 0 &&
           <DataTable removableSort paginator rows={10} stripedRows  value={patientList}>
               <Column field="patient_id" alignHeader={'center'} sortable header="id" hidden></Column>
               <Column field="first_name" alignHeader={'center'} sortable header="FirstName"></Column>
               <Column field="last_name" alignHeader={'center'} sortable header="LastName"></Column>
               <Column field="age" alignHeader={'center'} sortable header="Age"></Column>
           </DataTable>
+          }
       </div>
     </DoctorNav>
   )
