4851d07b5772d8bacd8278e8ba2c003e4eeea169
Ritish <ritishrish181@gmail.com>
2024-02-25 15:13:12 +0530

    added search functionality with prime

--- client/src/components/AdminAllPatients.jsx
+++ client/src/components/AdminAllPatients.jsx
@@ -1,36 +1,37 @@
 import React, { useState } from 'react';
-import '../style/AdminAllPatientStatus.css';
-import { Navbar } from './navbar';
-import axios from 'axios';
-import { useEffect } from 'react';
+import { InputText } from 'primereact/inputtext';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { approvedpatients } from '../services/services';
+import { Navbar } from './navbar';
+import '../style/AdminAllPatientStatus.css';
 
-export default function AdminAllPatient() { 
+export default function AdminAllPatient() {
+    const [globalFilter, setGlobalFilter] = useState('');
 
-    const [approvedList,setApprovedList]=useState([[]])
-    useEffect(() => {
-        const fetchData=()=>{
-            approvedpatients("http://localhost:5000/api/v1/admin/patient/status")
-            .then(response=>{
-                if(response.length==0) console.log("Error")
-                else setApprovedList(response.data)
-            })
-        }
-        fetchData();
-    }, [approvedList])
+    const statuses = [
+        { appId: 21, name: 'Shubham Singh', status: 'View' },
+        { appId: 456, name: 'Soofa Singh', status: 'View' },
+        { appId: 789, name: 'Bob Smith', status: 'View' },
+        { appId: 782, name: 'Steve Smith', status: 'View' },
+        { appId: 782, name: 'Virat Kohli', status: 'View' },
+        { appId: 782, name: 'Sanju Samson', status: 'View' }
+    ];
 
-  return (
+    const header = (
+        <div  style={{ textAlign: 'left' }}>
+            <i className="pi pi-search" style={{ margin: '4px 4px 0 0' }}></i>
+            <InputText id='search-bar' type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Search" />
+        </div>
+    );
+
+    return (
         <Navbar>
-          <div className='status'>
-          <h2 style={{textAlign:"center",color:"#00866E",display: "inline",
-                verticalAlign:"middle"}}>All Patients</h2>
-                    <DataTable removableSort paginator rows={10} value={approvedList}>
-                        <Column field="patient_id" alignHeader={'center'} sortable header="Patient ID"></Column>
-                        <Column field="first_name" alignHeader={'center'} sortable header="Name"></Column>
-                        <Column field="status" alignHeader={'center'} sortable header="Status"></Column>
-                    </DataTable>
+            <div className='status'>
+                <DataTable value={statuses} header={header} globalFilter={globalFilter}>
+                    <Column field="appId" header="App_ID" />
+                    <Column field="name" header="Name" />
+                    <Column field="status" header="Status" />
+                </DataTable>
             </div>
         </Navbar>
     );
